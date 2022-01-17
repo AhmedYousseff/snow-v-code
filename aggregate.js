9f781877a4dc1507c6fe376277032e5d46e95697
Ahmed Youssef <a_yousseff@icloud.com>
2022-01-17 10:53:02 +0100

    return total number of recordes inside table groupBy a field

--- aggregate.js
+++ aggregate.js
@@ -1,15 +1,15 @@
 /* return total number of record inside a table
-* using a second paramete in the addAggregate FUNCTION  will add groupedBy to the returned value
+* using a second groupBy FUNCTION  will to returned value by a field
 */
 (function() {
 	var count = new GlideAggregate('incident');
-	count.addAggregate('COUNT', 'state');
+	count.addAggregate('COUNT');
+	count.groupBy('state');
 	count.query();
 
 	while(count.next()) {
-		var counter = count.getAggregate('COUNT', 'state');
+		var counter = count.getAggregate('COUNT');
 		var state = count.state.getDisplayValue();
 		gs.info('count: {0}, state: {1}', counter, state);
 	}
-
 }());
